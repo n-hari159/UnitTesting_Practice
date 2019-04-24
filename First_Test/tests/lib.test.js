92b3efdc6473fed4875173b427fcc2e1e871a0d4
n-hari159 <n.hari159@gmail.com>
2019-04-25 00:36:03 +0530

    testing arrays

--- First_Test/tests/lib.test.js
+++ First_Test/tests/lib.test.js
@@ -24,3 +24,27 @@ describe('greet', () => {
         expect(result).toMatch(/Hari/); // using regular expressions to make it balanced(not to be too specific)
     });
 });
+
+describe('getCurrencies', () => {
+    it('should return supported currencies', () => {
+        const result = lib.getCurrencies();
+
+        // too general
+        expect(result).toBeDefined();
+        expect(result).not.toBeNull();
+
+        // too specific
+        expect(result[0]).toBe('USD');
+        expect(result[1]).toBe('AUD');
+        expect(result[2]).toBe('EUR');
+        expect(result.length).toBe(3);
+
+        // proper way
+        expect(result).toContain('USD');
+        expect(result).toContain('AUD');
+        expect(result).toContain('EUR');
+
+        // ideal way
+        expect(result).toEqual(expect.arrayContaining(['EUR', 'AUD', 'USD']));
+    });
+});
