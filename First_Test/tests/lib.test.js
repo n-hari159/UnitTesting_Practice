de23871e47b133179573b8c1a6cac45e4b1ed3da
n-hari159 <n.hari159@gmail.com>
2019-04-25 00:17:29 +0530

    testing numbers, grouping tests and refactoring

--- First_Test/tests/lib.test.js
+++ First_Test/tests/lib.test.js
@@ -1,4 +1,19 @@
+const lib = require('../lib');
 
-test('Our first test', () => {
+// describe is a function in jasmine/test where we can group all our related tests in single function. we replace test with it here.
+describe('absolute', () => {
+    it('should return a positive number if input is positive', () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
     
+    it('should return a positive number if input is negative', () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return a zero if input is zero', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
 });
